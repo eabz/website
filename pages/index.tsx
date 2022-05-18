a84510253875fc0fe94a9db0cb863e4ddc890f9e
eabz <eabz@kindynos.mx>
2022-05-18 14:36:45 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,3 @@
-import { Header } from "../components/Header";
-import { Footer } from "../components/Footer";
-import Link from "next/link";
-import Image from "next/image";
-
 export default function Home(): JSX.Element {
   return <></>;
 }
